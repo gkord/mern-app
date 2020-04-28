513268fb7f244f80234aafa5dd16985ff43ec82c
Gabe Kugelmass <gkord@me.com>
2020-04-28 15:22:11 -0400

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>SUP</div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
