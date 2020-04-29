82cdce62f6cb83858e5c147712bd7d5be9e0ec69
Gabe Kugelmass <gkord@me.com>
2020-04-29 18:10:17 -0400

    update linting and frontend structure

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -22,24 +22,25 @@ router.route('/add').post((req, res) => {
     date,
   });
 
-  newExercise.save()
+  newExercise
+    .save()
     .then(() => res.json('Exercise added!'))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.route('/:id').get((req,res) =>{
+router.route('/:id').get((req, res) => {
   Exercise.findById(req.params.id)
     .then((exercise) => res.json(exercise))
     .catch((err) => res.status(400).json(`Error: ${err}`));
-})
+});
 
 router.route('/:id').delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Exercise deleted."))
+    .then(() => res.json('Exercise deleted.'))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.route('/update/:id').post((req,res) => {
+router.route('/update/:id').post((req, res) => {
   Exercise.findById(req.params.id)
     .then((exercise) => {
       exercise.username = req.body.username;
@@ -47,11 +48,12 @@ router.route('/update/:id').post((req,res) => {
       exercise.duration = Number(req.body.duration);
       exercise.date = Date.parse(req.body.date);
 
-      exercise.save()
+      exercise
+        .save()
         .then(() => res.json('Exercise updated!'))
         .catch((err) => res.status(400).json(`Error: ${err}`));
     })
     .catch((err) => res.status(400).json(`Error: ${err}`));
-})
+});
 
 module.exports = router;
--- frontend/src/components/CreateExercise/CreateExercise.js
+++ frontend/src/components/CreateExercise/CreateExercise.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button,
+  FormGroup,
+  FormControl,
+  FormText,
+  Dropdown,
+  DropdownDivider,
+  DropdownItem,
+  DropdownMenu,
+} from 'styled-bootstrap-components';
 
 const CreateExercise = () => {
-  return <div>Create Exercise Component</div>;
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form>
+        <FormGroup>
+          <label>Username</label>
+          <Dropdown>
+            <Button
+              dropdownToggle
+              onClick={() => setHidden(!hidden)}
+            >
+              Dropdown Button
+            </Button>
+            <DropdownMenu
+              hidden={hidden}
+              toggle={() => setHidden(!hidden)}
+            >
+              <DropdownItem>Action</DropdownItem>
+              <DropdownItem>Another action</DropdownItem>
+              <DropdownDivider />
+              <DropdownItem>Action after divider</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <FormText muted>
+            We&#39;ll never share your email with anyone else.
+          </FormText>
+        </FormGroup>
+      </form>
+    </div>
+  );
 };
 
 export default CreateExercise;
